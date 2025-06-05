c5b58e787ad906ef420493b9ed084119f844140c
Gilberto Comas <gilbertocomas@gmail.com>
2025-06-05 17:04:08 -0400

    Agregando enlace video youtube

--- src/views/ExperienciaPage.vue
+++ src/views/ExperienciaPage.vue
@@ -5,32 +5,35 @@
         <ion-buttons slot="start">
           <ion-menu-button color="primary"></ion-menu-button>
         </ion-buttons>
-        <ion-title>{{titulo}}</ion-title>
+        <ion-title>{{ titulo }}</ion-title>
       </ion-toolbar>
     </ion-header>
 
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">{{titulo}}</ion-title>
+          <ion-title size="large">{{ titulo }}</ion-title>
         </ion-toolbar>
       </ion-header>
 
       <div id="container">
         <strong class="capitalize">Experiencia IONIC</strong>
         <p>
-          En el siguiente video explico como fue mi experiencia al desarrollar la Tarea No. 3 en la asignatura Introducción al Desarrollo de Aplicaciones Móviles,
-          impartida por el maestro Amadis Suarez. 
+          En el siguiente video explico como fue mi experiencia al desarrollar
+          la Tarea No. 3 en la asignatura Introducción al Desarrollo de
+          Aplicaciones Móviles, impartida por el maestro Amadis Suarez.
         </p>
-        <iframe 
-          width="100%" 
-          height="315" 
-          src="https://www.youtube.com/embed/_NZBWI94MkA" 
-          title="Tarea III IONIC" 
-          frameborder="0" 
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-          referrerpolicy="strict-origin-when-cross-origin" 
-          allowfullscreen>
+
+        <iframe
+          width="100%"
+          height="315"
+          src="https://www.youtube.com/embed/dNnR2Z2UOXs"
+          title="Tarea III"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerpolicy="strict-origin-when-cross-origin"
+          allowfullscreen
+        >
         </iframe>
       </div>
     </ion-content>
@@ -38,7 +41,6 @@
 </template>
 
 <script setup lang="ts">
-
 const titulo = "Experiencia";
 
 import {
@@ -80,6 +82,6 @@ import {
 }
 
 iframe {
-  aspect-ratio: 16/9; 
+  aspect-ratio: 16/9;
 }
 </style>
